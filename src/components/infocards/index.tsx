fc81e6098eac592f01a78f5d1ff88892976155d1
victorpbp <victorrpeixotoo@gmail.com>
2023-06-26 17:20:59 -0300

    chore: fixing names/imports/code disposition

--- src/components/infocards/index.tsx
+++ src/components/infocards/index.tsx
@@ -10,6 +10,17 @@ export default function BlueCards(props: BlueCardsProps) {
 
   const { children } = props;
   return (
-    <Center cursor="pointer" textAlign={'center'} h="110px" w="360px" padding={"110px 180px"} bg="#8FC3FF" borderRadius="10px" fontWeight={"500"} fontSize="40px">{children}</Center>
+    <Center 
+    cursor="pointer" 
+    textAlign={'center'} 
+    h="110px" 
+    w="360px" 
+    padding={"110px 180px"} 
+    bg="#8FC3FF" 
+    borderRadius="10px" 
+    fontWeight={"500"} 
+    fontSize="40px">
+      {children}
+    </Center>
   );
 }
